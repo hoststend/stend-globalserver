6401d62a6da4d21f2243d21bdc34e3441ed9fb01
Johan <johan@johanstick.fr>
2025-03-20 20:01:27 +0100

    eslint

--- utils/profanity.js
+++ utils/profanity.js
@@ -1,17 +1,17 @@
-var fs = require('fs')
-var path = require('path')
+var fs = require("fs")
+var path = require("path")
 var profanities = []
 
 // https://github.com/darwiin/french-badwords-list/blob/master/list.txt
 
 module.exports = (string) => {
 	if(!profanities.length) {
-		profanities = fs.readFileSync(path.join(__dirname, 'profanities.txt')).toString().split('\n')
+		profanities = fs.readFileSync(path.join(__dirname, "profanities.txt")).toString().split("\n")
 		profanities = profanities.map(profanity => profanity.trim().toLowerCase())
 	}
 
 	profanities.forEach(profanity => {
-		string = string.replace(new RegExp(profanity, 'gi'), '*'.repeat(profanity.length))
+		string = string.replace(new RegExp(profanity, "gi"), "*".repeat(profanity.length))
 	})
 
 	return string
